e3f93e75ca0139c9ba276fc9e09be443c24c02d3
Maitreya-22 <55971904+Maitreya-22@users.noreply.github.com>
2021-04-18 20:53:55 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,56 @@
+
+<footer>
+    <div class="container-footer">
+        <div class="row">
+            <div class="footer-col">
+                <h5>Company</h5>
+                <ul>
+                    <li><a href="">About us</a></li>
+                    <li> <a href="">Our Services</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Affiliate Program</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h5>Get Help</h5>
+                <ul>
+                    <li><a href="">FAQ</a></li>
+                    <li><a href="">Shipping</a></li>
+                    <li><a href="">Returns</a></li>
+                    <li><a href="">Order Status</a></li>
+                    <li><a href="">Payment Options</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h5>Online Shop</h5>
+                <ul>
+                    <li>
+                        <a href="">Pizza</a>
+                    </li>
+                    <li>
+                        <a href="">Burger</a>
+                    </li>
+                    <li>
+                        <a href="">Pasta</a>
+                    </li>
+                    <li>
+                        <a href="">Coke</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h5>Follow us</h5>
+                <div class="social-links">
+                    <a href=""><i class="fab fa-facebook-f" style="padding-top: 10px;"></i></a>
+                    <a href=""><i class="fab fa-twitter" style="padding-top: 10px;"></i></a>
+                    <a href=""><i class="fab fa-instagram" style="padding-top: 10px;"></i></a>
+                    <a href=""><i class="fab fa-github" style="padding-top: 10px;"></i></a>
+                </div>
+                        
+            </div>
+        </div>
+
+    </div>
+</footer>
+</body>
+</html>
